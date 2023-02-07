822fe6d663a75b5833b5bdebe5ca30e2cecef13c
Srinivas Elimilla <elimillasrinivas@gmail.com>
2023-02-07 16:43:00 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import { useState } from 'react';
+import './app.css'
+function App() {
+  const [inp,setInp]=useState("")
+  const [otp,setOtp]=useState("")
+  const [otpColor,setOtpColor]=useState("")
+  const handlePolindrome=()=>{
+    // console.log(inp);
+    let s1=inp.replace(/[^a-zA-Z0-9]/g,'').toLowerCase()
+    let s2=''
+    for(let i=s1.length-1;i>=0;i--){
+      s2+=s1[i]
+    }
+    if(s1===s2){
+      setOtp("Yes! It is a Polindrome.")
+      setOtpColor("green")
+      
+    }
+    else{
+      setOtp("No, It is not a Polindrome.")
+      setOtpColor("red")
+    }
+    
+  setInp("")
+  }
+  return (
+    <>
+      <div className="app">
+        <h1 className="header">Polindrome Checker..!</h1>
+        <div className="inp-container">
+          <input
+            type="text"
+            className="inp-text"
+            placeholder=" Enter a word/sentence "
+            onChange={(e) => {
+              setInp(e.target.value);
+            }}
+          />
+          <button className="inp-text-btn" onClick={handlePolindrome}>
+            Check
+          </button>
+        </div>
+        <h1 className="otp" style={{color:`${otpColor}`}}>{otp}</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
